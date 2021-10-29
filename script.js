5a9552f8e144bbad074adbf91ce31b3f25dc3c15
reforgedreinvigorated <briantuquoc.le@gmail.com>
2021-10-29 15:20:08 -0500

    completed ui of calculator.

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+const numpads = document.querySelectorAll(".numpad");
+let mathDisplay = document.getElementById("mathDisplay");
+let currNumberStr = document.getElementById("currNumber");
+// if currNum is set to -1 to indicate it is empty by default (no number has been inputed)
+let currNum = -1;
