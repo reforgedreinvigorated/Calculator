8c0c509c8c130f33d04b378eb8b9a3c909f9305d
reforgedreinvigorated <briantuquoc.le@gmail.com>
2021-10-29 22:35:15 -0500

    added initial implementation to finding out which button was pressed on the calculator

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ let mathDisplay = document.getElementById("mathDisplay");
 let currNumberStr = document.getElementById("currNumber");
 // if currNum is set to -1 to indicate it is empty by default (no number has been inputed)
 let currNum = -1;
+
+numpads.forEach(numpad => numpad.addEventListener("click", function(e)
+{
+    console.log(numpad.textContent);
+}));
